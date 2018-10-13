87769a3bdb27c60af23bf9780a6ce55512a50f5a
AomineDaike <renjy0923@outlook.com>
2018-10-13 10:37:25 +0800

    #smart-framework daike update.

---
+++ src/main/java/org/smart4j/framework/annotation/Controller.java
@@ -0,0 +1,17 @@
+package org.smart4j.framework.annotation;
+
+/**
+ * @ProjectName: smart-framework
+ * @Package: org.smart4j.framework.annotation
+ * @ClassName: ${TYPE_NAME}
+ * @Description: java类作用描述
+ * @Author: Ren, Jianyong.
+ * @CreateDate: 2018/10/13 10:37
+ * @UpdateUser: Ren, Jianyong.
+ * @UpdateDate: 2018/10/13 10:37
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ * <p>Copyright: Copyright (c) 2018</p>
+ */
+public @interface Controller {
+}
--- src/main/java/org/smart4j/framework/util/ClassUtil.java
+++ src/main/java/org/smart4j/framework/util/ClassUtil.java
@@ -3,6 +3,11 @@ package org.smart4j.framework.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -38,6 +43,54 @@ public final class ClassUtil {
     }
 
     public static Set<Class<?>> getClassSet(String packageName) {
-        return null;
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        try {
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (url != null) {
+                    String protocol = url.getProtocol();
+                    if (protocol.equals("file")) {
+                        String packagePath = url.getPath().replaceAll("%20", " ");
+
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("Get Class Set Failure!", e);
+            throw new RuntimeException(e);
+        }
+        return classSet;
+    }
+
+    private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
+        File[] files = new File(packagePath)
+            .listFiles(file -> (file.isFile() && file.getName().endsWith(".class") || file.isDirectory()));
+
+        for (File file : files) {
+            String fileName = file.getName();
+            if (file.isFile()) {
+                String className = fileName.substring(0, fileName.lastIndexOf("."));
+                if (StringUtil.isNotEmpty(packageName)) {
+                    className = packageName + "." + className;
+                }
+                doAddClass(classSet, className);
+            } else {
+                String subPackagePath = fileName;
+                if (StringUtil.isNotEmpty(packagePath)) {
+                    subPackagePath = packagePath + "/" + subPackagePath;
+                }
+                String subPackageName = fileName;
+                if (StringUtil.isNotEmpty(packageName)) {
+                    subPackageName = packageName + "." + subPackageName;
+                    addClass(classSet, subPackagePath, subPackageName);
+                }
+            }
+        }
+    }
+
+    private static void doAddClass(Set<Class<?>> classSet, String className) {
+        Class<?> aClass = loadClass(className, false);
+        classSet.add(aClass);
     }
 }
